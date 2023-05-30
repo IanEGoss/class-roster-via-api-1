78bf11d5aeb17e14e9cf2b48840b6930e557abb1
IanEGoss <97562701+IanEGoss@users.noreply.github.com>
2023-05-30 21:32:50 +0000

    data js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+/*
 // My Scripts
 
 
@@ -37,3 +38,36 @@ fetch(url)
       gallery.insertAdjacentHTML("afterbegin", template);
     });
   });
+*/
+// Data source
+const dataSource = "https://assets.codepen.io/16425/spring23web3.json";
+
+// fetch
+fetch( dataSource )
+.then( response => response.json())
+.then( data  => {
+
+    // get container for data
+    const roster = document.querySelector(".roster");
+ 
+    // for each RECORD, insert into parent
+    data.forEach( (student) => { 
+      
+      // template to reuse
+      const template = `
+        <figure>
+         <figcaption> ${student.Name} </figcaption>
+            <img src="${student.Image}" alt="Student Name">
+         </figure>
+      `
+    
+      roster.insertAdjacentHTML('afterbegin', template );
+      
+    });
+  
+});
+
+    // console.log(data);
+    // console.log(data[9].Superpower);
+    // console.log(data[2].Emoji);
+    // console.log(data[1].Quote);
